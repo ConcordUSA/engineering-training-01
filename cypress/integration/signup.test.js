162736efde4b058b0cfb1389e7e22e3df4efe02a
Adam Schnaare <1749981+adamschnaare@users.noreply.github.com>
2021-02-04 16:57:52 -0600

    test(signup): Cleanup signup test

--- cypress/integration/signup.test.js
+++ cypress/integration/signup.test.js
@@ -2,45 +2,35 @@
 import faker from "faker";
 
 context("Signup", () => {
-  // successful login
-  // unsuccessful registration (not-unique email)
-  // unsuccessful registration (missing required fields)
-  // unsuccessful login (missing required fields)
-
   // successful registration
-  it("should register successfully when given required data", () => {
+  it("should register successfully given required data", () => {
     const password = faker.internet.password();
-    //signup:
-    //navigate to page
     cy.visit("/");
-    //find login button
-    //click login
     cy.get("#loginButton").click();
     cy.location("pathname").should("equal", "/login");
-    //click create account button
     cy.get("#createAccountButton").click();
     cy.location("pathname").should("equal", "/createAccount");
-    //enter first name
     cy.get("#firstName").type(faker.name.firstName());
-    //enter last name
     cy.get("#lastName").type(faker.name.lastName());
-    //enter email
     cy.get("#email").type(faker.internet.email());
-    //enter company
     cy.get("#company").type(faker.company.companyName());
-    //enter phone(s)
     cy.get("#companyPhone").type(faker.phone.phoneNumber());
     cy.get("#personalPhone").type(faker.phone.phoneNumber());
-    //enter password
     cy.get("#password").type(password);
-    //enter password confirmation
     cy.get("#passwordConfirm").type(password);
-    //click register button
     cy.get("#registerButton").click();
-    //back to home
     cy.location("pathname").should("equal", "/");
 
     // CLEANUP
     // TODO: delete the user that was made (research on how to clear all data from emulators)
   });
+
+  // unsuccessful registration (not-unique email)
+  it.skip("should not register successfully given bad data", () => {});
+  // unsuccessful registration (missing required fields)
+  it.skip("should not register successfully given missing data", () => {});
 });
+
+// TODO Seed db with login info
+// successful login
+// unsuccessful login (missing required fields)
