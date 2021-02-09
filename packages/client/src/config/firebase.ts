d50e96feaf94912bde865216a9b972eccc5a6b9b
Adam Schnaare <1749981+adamschnaare@users.noreply.github.com>
2021-02-09 14:49:03 -0600

    ci: Adjust NODE_ENV in builds

--- packages/client/src/config/firebase.ts
+++ packages/client/src/config/firebase.ts
@@ -6,7 +6,7 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
-import getConfigs from ".";
+import getConfigs from "./index";
 
 const { useEmulators } = getConfigs();
 
