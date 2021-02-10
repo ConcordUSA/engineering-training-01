2399d6f12502c20a069fd596c4653f7d8ba3e8ec
longchamp-concord <max.longchamp@concordusa.com>
2021-02-10 15:49:20 -0600

    feat et1-39 (signout button): adds signout as component to menubar

--- packages/client/src/views/Menubar.tsx
+++ packages/client/src/views/Menubar.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import SignOut from "./signout";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  
+
   title: {
     flexGrow: 1,
   },
@@ -20,12 +21,12 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar 
-      position="sticky" 
-      style={{backgroundColor:"#FF505F", 
-      color: "#FF505F"}}>
+      <AppBar
+        position="sticky"
+        style={{ backgroundColor: "#FF505F", color: "#FF505F" }}
+      >
         <Toolbar>
-        <Button variant="contained" color="inherit">Sign-out</Button>
+          <SignOut />
           <Avatar src="/broken-image.jpg" />
         </Toolbar>
       </AppBar>
