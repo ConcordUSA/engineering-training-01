7b25e046327169f3b6160ce538b6fc96877d37cc
thomas hoffmann <thomas.hoffmann@concordusa.com>
2021-02-11 10:52:57 -0600

    test: adds emulators to client tests

--- packages/client/tests/eventsService.test.ts
+++ packages/client/tests/eventsService.test.ts
@@ -1,23 +1,17 @@
 import { getFirestore } from "../src/config/firebase";
-import EventService from "../src/services/eventService";
-import { Event } from "../src/models/event";
+import EventsService from "../src/services/eventsService";
+import { Event, EventFactory } from "../src/models/event";
 import firebase from "firebase-admin";
 
 describe("EventsService", () => {
   const db = getFirestore();
-  const service = new EventService(db);
+  const service = new EventsService(db);
   //   const admin = firebase.initializeApp();
   //   const adminDb = admin.firestore();
 
   it("should create and retrieve an event", async () => {
     // given
-    const event: Event = {
-      topic: "topic",
-      location: "location",
-      date: new Date(),
-      category: ["category"],
-      price: 12345,
-    };
+    const event = EventFactory();
 
     // when
     const id = await service.createEvent(event);
