40758c4a722da3cc2a4c7836f2be83c9cc756e46
Adam Schnaare <1749981+adamschnaare@users.noreply.github.com>
2021-02-11 09:00:29 -0600

    feat: Adds menubar

--- packages/client/src/views/Menubar.tsx
+++ packages/client/src/views/Menubar.tsx
@@ -1,33 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import Avatar from "@material-ui/core/Avatar";
-import SignOut from "./signout";
-
+import { AppDependencies, AppDependenciesContext } from "../appDependencies";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-
   title: {
     flexGrow: 1,
   },
+  menuBar: {
+    backgroundColor: "#FF505F",
+  },
+  avatar: {
+    position: "fixed",
+    right: "8px",
+  },
+  signOutBtn: {
+    position: "fixed",
+    right: "8%",
+  },
 }));
-
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const { auth }: AppDependencies = useContext(AppDependenciesContext);
+
+  const handleSignOut = async () => {
+    console.log("attempting to signout");
+    await auth.signOut();
+  };
 
   return (
     <div className={classes.root}>
       <AppBar
-        position="sticky"
         style={{ backgroundColor: "#FF505F", color: "#FF505F" }}
+        className={classes.menuBar}
       >
         <Toolbar>
-          <SignOut />
-          <Avatar src="/broken-image.jpg" />
+          <Button
+            variant="contained"
+            className={classes.signOutBtn}
+            onClick={handleSignOut}
+          >
+            Signout
+          </Button>
+          <Avatar src="/broken-image.jpg" className={classes.avatar} />
         </Toolbar>
       </AppBar>
     </div>
