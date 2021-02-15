c167dec1f422b747f2c8d47c1239ad1b1bd45f53
Adam Schnaare <1749981+adamschnaare@users.noreply.github.com>
2021-02-15 15:40:11 -0600

    fix: Removes users password from db

--- packages/client/src/services/usersService.ts
+++ packages/client/src/services/usersService.ts
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 import { User, UserFactory } from "../models/user";
 
-export default class UsersService {  
-  private collection = "users"
+export default class UsersService {
+  private collection = "users";
 
   constructor(
     private db: firebase.firestore.Firestore,
-    private auth?: firebase.auth.Auth,    
+    private auth?: firebase.auth.Auth
   ) {}
 
   public createUser = async (user: User) => {
@@ -20,11 +20,15 @@ export default class UsersService {
       user.email,
       user.password
     );
-    uid = userCredential?.user?.uid;;
+    uid = userCredential?.user?.uid;
     // create user in db service
+    delete user.password;
     try {
-      await this.db.collection(this.collection).doc(uid).set({...user, uid });
-      return {uid, message:"User successfully created"};
+      await this.db
+        .collection(this.collection)
+        .doc(uid)
+        .set({ ...user, uid });
+      return { uid, message: "User successfully created" };
     } catch (error) {
       return { error: error.message };
     }
@@ -34,17 +38,19 @@ export default class UsersService {
     const doc = await this.db.collection(this.collection).doc(uid).get();
     const data = doc.data() as User;
     const user = UserFactory(data);
-  
+
     return user;
   }
 
   public async updateUser(uid: string, data: Object) {
     try {
-      await this.db.collection(this.collection).doc(uid).set({...data}, {merge:true});
-      return {uid, message:"User successfully updated"};      
+      await this.db
+        .collection(this.collection)
+        .doc(uid)
+        .set({ ...data }, { merge: true });
+      return { uid, message: "User successfully updated" };
     } catch (error) {
       return { error: error.message };
     }
   }
-
 }
