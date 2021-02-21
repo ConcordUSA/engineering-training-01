fe30f83cc3d75c23b0935f7aedc466b903d356a7
c-conklin <16570894+c-conklin@users.noreply.github.com>
2021-02-21 07:03:25 -0700

    feat(search): greatly improved event search

--- packages/client/src/views/EventList.tsx
+++ packages/client/src/views/EventList.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useMemo, useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { Event, capitalize } from "../models/event";
+import { IEvent, capitalize } from "../models/event";
 import UsersService from "../services/usersService";
 import { AppDependencies, AppDependenciesContext } from "../appDependencies";
 import EventsService, { EventsPerCategory } from "../services/eventsService";
@@ -10,6 +10,7 @@ import Grid from "@material-ui/core/Grid";
 import AppTheme from "../styles/theme";
 import { useRecoilState } from "recoil";
 import { searchTerm } from "../store";
+import { colors } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,23 +25,25 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingTop: "20px",
     },
     categoryHeader: {
-      fontSize: "3.2em",
+      fontSize: "2.2em",
+      paddingLeft: "6vh",
+      color: colors.grey[500],
+      fontWeight: 500,
     },
   })
 );
 
-const searchFilter = (search: string, events: EventsPerCategory[]): any => {
-  let res = [];
-  const searchTermArray = [
-    search,
-    search.toLowerCase(),
-    search.toUpperCase(),
-    capitalize(search.toLowerCase()),
-  ];
+const searchFilter = async (search: string, events: EventsPerCategory[]) => {
+  let res: EventsPerCategory[] = [];
   events.forEach((eventCategory: EventsPerCategory) => {
-    eventCategory.items = eventCategory.items.filter((event: Event) => {
-      const topicWords = event.topic.split(" ");
-      return searchTermArray.some((i) => topicWords.includes(i));
+    const regex = new RegExp(`^${search}`, "gi");
+    eventCategory.items = eventCategory.items.filter((event: IEvent) => {
+      let i = 0;
+      const keyWords = event.topic.split(" ");
+      while (keyWords[i]) {
+        if (keyWords[i++].match(regex)) return true;
+      }
+      return false;
     });
     res.push(eventCategory);
   });
@@ -66,10 +69,10 @@ export default function EventListView() {
     if (user?.interestedCategories) {
       eventService
         .getAllEvents(user.interestedCategories)
-        .then((eventList: EventsPerCategory[]) => {
+        .then(async (eventList: EventsPerCategory[]) => {
           setEvents(
             searchTermState
-              ? searchFilter(searchTermState, eventList)
+              ? await searchFilter(searchTermState, eventList)
               : eventList
           );
         });
@@ -93,7 +96,7 @@ export default function EventListView() {
               justify="center"
               alignItems="flex-start"
             >
-              {categoryList?.items?.map((event: Event | any) => (
+              {categoryList?.items?.map((event: IEvent | any) => (
                 <Events event={event} key={event.topic} />
               ))}
             </Grid>
