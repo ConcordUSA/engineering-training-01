d1c29a4409eefcdb2f2704cd9d1a22ad233d77ea
longchamp-concord <max.longchamp@concordusa.com>
2021-02-24 17:14:29 -0600

    feat (admin view attendees): -adds functionality for admin users to view details about event guests -includes a refactor of non-admin viewing of event guests as well

--- packages/client/src/views/ViewAttendees.tsx
+++ packages/client/src/views/ViewAttendees.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useMemo, useContext } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { Button, Modal } from "@material-ui/core";
+import { Button, Modal, List } from "@material-ui/core";
 import AppTheme, { materialTheme } from "../styles/theme";
 import EventsService from "../services/eventsService";
 import UsersService from "../services/usersService";
 import { AppDependencies, AppDependenciesContext } from "../appDependencies";
 import { User } from "../models/user";
+import ListItemComponent from "./ListItemComponent";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -52,13 +53,13 @@ export default function ViewAttendees(props) {
   const eventService = useMemo(() => new EventsService(db), [db]);
   const usersService = useMemo(() => new UsersService(db), [db]);
   const [eventAttendees, setEventAttendees] = useState<User[]>([]);
-  const [user, setUser] = useState<User>();
+  const [isAdmin, setIsAdmin] = useState(false);
   const [open, setOpen] = useState(false);
 
   async function handleViewAttendees() {
     const attendees = await eventService.getAttendees(props.event);
     const user = await usersService.getUser(auth.currentUser.uid);
-    setUser(user);
+    setIsAdmin(user.isAdmin);
     setEventAttendees(attendees);
     setOpen(true);
   }
@@ -75,24 +76,23 @@ export default function ViewAttendees(props) {
           className={classes.secondaryBtn}
           onClick={handleViewAttendees}
         >
-          View Attendees
+          View Guests
         </Button>
 
         <Modal open={open} onClose={handleClose}>
           <div className={classes.paper}>
-            <h2 id="simple-modal-title">Registered Attendees</h2>
-            {eventAttendees.map((attendee) => {
-              return (
-                <div key={attendee.uid}>
-                  <p>{attendee.firstName + " " + attendee.lastName}</p>
-                  <div className={classes.indented}>
-                    <p> {user?.isAdmin && "    " + attendee.email}</p>
-                    <p> {user?.isAdmin && "    " + attendee.companyPhone}</p>
-                    <p> {user?.isAdmin && "    " + attendee.company}</p>
-                  </div>
-                </div>
-              );
-            })}
+            <h2 id="simple-modal-title">Registered Guests</h2>
+            <List>
+              {eventAttendees.map((attendee) => {
+                return (
+                  <ListItemComponent
+                    key={attendee.uid}
+                    attendee={attendee}
+                    isAdmin={isAdmin}
+                  />
+                );
+              })}
+            </List>
           </div>
         </Modal>
       </div>
