5b0e702e3cff3f6107322b633ed196a3be6380ec
CooperDoten <cooper.doten@gmail.com>
2021-02-24 08:58:38 -0600

    refactor(modal): removed uneccesary import

--- packages/client/src/views/SimpleModal.tsx
+++ packages/client/src/views/SimpleModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import FilterListIcon from "@material-ui/icons/FilterList";
