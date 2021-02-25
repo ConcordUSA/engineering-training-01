34eb6441ebee61bdbd975fecf3038d0bc41f1709
manisai <mani.sai@concordusa.com>
2021-02-25 11:12:38 -0600

    refactor:renamed create evebt form

--- packages/client/src/views/AuthenticatedViews.tsx
+++ packages/client/src/views/AuthenticatedViews.tsx
@@ -3,7 +3,7 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Route, Switch } from "react-router-dom";
 import routes from "../constants/routes";
 import EventDetailsView from "./EventDetails";
-import CreateEventView from "./CreateEvent";
+import EventDetailsFormView from "./EventDetailsForm";
 import EventListView from "./EventList";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -28,7 +28,7 @@ export default function App() {
         <Route
           exact
           path={routes.CREATE_EVENT_URL}
-          component={CreateEventView}
+          component={EventDetailsFormView}
         />
         <Route
           exact
@@ -38,8 +38,8 @@ export default function App() {
         <Route
           exact
           path={routes.EVENT_DETAILS_EDIT_URL}
-          component={CreateEventView}
-        ></Route>
+          component={EventDetailsFormView}
+        />
         <Route component={EventListView} />
       </Switch>
     </div>
--- packages/client/src/views/EventDetailsForm.tsx
+++ packages/client/src/views/EventDetailsForm.tsx
@@ -139,7 +139,7 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function CreateEventView() {
+export default function EventDetailsFormView() {
   const classes = useStyles();
   const newEvent = EventFactory();
   const history = useHistory();
